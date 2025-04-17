5d4fe2c9d4846e0a1510bd751a6d8901cad969d6
kulm2223@gmail.com <kulm2223@gmail.com>
2025-04-17 13:58:47 +0700

    Bug In Root & Realease Version

---
+++ src/pages/profil.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import Wrapper from "../layout/wrapper";
+import SEO from "../common/seo";
+import Profil from "../components/account/create-profil";
+
+const Index = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const imagePath = "../../public/assets/video/1.gif";
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Wrapper>
+      <SEO pageTitle={"Create Profil"} />
+      {isLoading ? (
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "50%", // Sesuaikan dengan lebar video
+            height: "50%" // Sesuaikan dengan proporsi asli video
+          }}
+        >
+          {/* Gunakan konstanta imagePath */}
+          <img src={imagePath} alt="" />
+        </div>
+      ) : (
+        <Profil />
+      )}
+    </Wrapper>
+  );
+};
+
+export default Index;
